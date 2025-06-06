3932aaa6625e7b95c3926a6cd1ebba9f6795fbd0
Hiroaki SHIBUKI <hidori@users.noreply.github.com>
2025-06-06 20:56:02 +0900

    initial commit

---
+++ Blnk-Kai/Blnk-Kai.ino
@@ -0,0 +1,37 @@
+// Blink-Kai
+
+#include <Arduino.h>
+
+// #undef LED_BUILTIN
+#define LED_PIN LED_BUILTIN
+// #define LED_PIN 3
+// #define LED_PIN 30 // Pro Micro TX
+// #define LED_PIN A3 // Pro Micro with 5P probe
+// #define LED_PIN PIN_PB3 // ATtiny85
+// #define LED_PIN PIN_PB3  // ATtiny13
+// #define LED_PIN 17 // Pro Micro RP2040
+// #define LED_PIN 8 // ESP32-C3 SuperMini
+// #define LED_PIN 8 // ESP33-C3 OLED SuperMini
+// #define LED_PIN PC1 // nanoCH32V003
+// #define LED_PIN PC1 // CH32V003
+
+#define LED_ON HIGH
+#define LED_OFF LOW
+// #define LED_ON LOW // Pro Micro TX
+// #define LED_OFF HIGH // Pro Micro TX
+
+// #define LED_WAIT 100
+#define LED_WAIT 200
+// #define LED_WAIT 1000
+// #define LED_WAIT 3000
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, LED_ON);
+  delay(LED_WAIT);
+  digitalWrite(LED_PIN, LED_OFF);
+  delay(LED_WAIT);
+}
